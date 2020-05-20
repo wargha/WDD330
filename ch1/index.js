72fa3fab8e6665649dd3cbaad9c1e7c52896a25d
Lucas Wargha <lucas.wargha@Lucas-Macbook.local>
2020-05-20 10:22:46 -0600

    ch1

--- ch1/index.js
+++ ch1/index.js
@@ -1,7 +1,9 @@
 var tasksRef = db.collection("tasks");
 var currentTasks = [];
-var activeFilter = "active";
+var activeFilter = "all";
+
 window.onload = loadTasks();
+
 function loadTasks() {
   currentTasks = [];
   tasksRef //vamos pegar as tasks da nossa db
@@ -15,21 +17,32 @@ function loadTasks() {
     })
     .then(() => {
       //se chegamos aqui e pq terminamos nossa query, e ja podemos colocar todas as tasks na tela! :)
-      document.getElementById("todos").innerHTML = ""; //need to reset html so the container can receive new data without repeating it
-      currentTasks.forEach((task) => {
-        document.getElementById("todos").insertAdjacentHTML(
-          "beforeend",
-          `<div class="container row single-todo">
-      <div onclick="toggleState('${task?.id}')" class="input hover ${
-            task.completed ? "completed" : ""
-          }"></div>
-      <span class="task-name">${task?.name}</span>
-      <span onclick="deleteTask('${task?.id}')" class="remove-task">X</span>
-    </div>`
-        );
-      });
+      updateTasks();
     });
 }
+
+const updateTasks = () => {
+  document.getElementById("todos").innerHTML = ""; //need to reset html so the container can receive new data without repeating it
+  currentTasks.forEach((task) => {
+    if (activeFilter === "completed" && !task.completed) {
+      return;
+    } else if (activeFilter === "active" && task.completed) {
+      return;
+    }
+    document.getElementById("todos").insertAdjacentHTML(
+      "beforeend",
+      `<div class="container row single-todo">
+  <div onclick="toggleState('${task?.id}')" class="input hover ${
+        task?.completed ? "completed" : ""
+      }"></div>
+  <span class="task-name ${task?.completed ? "completed" : ""}">${
+        task?.name
+      }</span>
+  <span onclick="deleteTask('${task?.id}')" class="remove-task">X</span>
+</div>`
+    );
+  });
+};
 
 const toggleState = (id) => {
   let completed = [...currentTasks]?.filter((task) => task?.id === id)[0]
@@ -54,12 +67,27 @@ const deleteTask = (id) => {
 };
 
 const addTask = () => {
-  tasksRef.add({ name: "Lucas", completed: false }).then(() => {
+  let name = document.getElementById("new-task-input").value;
+  tasksRef.add({ name, completed: false }).then(() => {
+    document.getElementById("new-task-input").value = "";
     loadTasks();
   });
 };
 
 const changeFilter = (e, newFilter) => {
-  console.log(e);
   activeFilter = newFilter;
+  let els = document.getElementsByClassName("active-filter");
+  els[0].classList.remove("active-filter");
+  e.srcElement.classList.add("active-filter");
+  updateTasks();
+};
+
+const validateInput = () => {
+  let input = document.getElementById("new-task-input").value;
+  console.log(input);
+  if (input === "") {
+    document.getElementById("submitNewTask").classList.add("hidden");
+  } else {
+    document.getElementById("submitNewTask").classList.remove("hidden");
+  }
 };
